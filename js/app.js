9bae006e39b33eb7b5e6c59383115a2976dc08d1
Travis <travisjw@gmail.com>
2018-05-21 11:35:51 -0700

    Added in Game Arrays and pushing to them.

--- js/app.js
+++ js/app.js
@@ -1,4 +1,11 @@
 'use strict';
+//Arrays of Stuff
+var assortedGameArrays{
+    enemies: [],
+    bullets: [],
+    playerBullets: [],
+}
+
 
 var Player = {
     xPosition: 'Canvas /2 ',
@@ -11,10 +18,11 @@ function Enemy (xPosInitial, xVelocityInitial, xCenterpoint, yPosInital, yVeloci
     this.yPoxition = yPosInital;
     this.xVelocity = xVelocityInitial;
     this.yVelocity = yVelocityInitial;
-    this.xCenterpoint = xCenterpoint;  //  Using a f= -kx model to change velocity.  Possibly with random numbers, because bad-guys are trying to dodge bullets too.
+    this.xCenterpoint = xCenterpoint;  //  Using a f=-kx model to change velocity.  Possibly with random numbers, because bad-guys are trying to dodge bullets too.
     this.yCenterpoint = yCenterpoint;
     this.image = image;
     this.type = type;
+    assortedGameArrays.enemies.push(this);
 };
 
 function EnemyBullet (xPosInitial, yPosInital, totalVelocity){
@@ -22,12 +30,14 @@ function EnemyBullet (xPosInitial, yPosInital, totalVelocity){
     this.yPoxition = yPosInital;
     this.xVelocity = 0;  //  Math.sqrt(Math.pow(totalVelocity,2) + math.pow(player.xPosition-xPosInitial,2));  // Pretty sure this works, but should test with numbers to make sure before implementing.   Alternately, can just have them shoot straight down the Y axis like every other bullet.
     this.yVelocity = totalVelocity; // Math.sqrt(Math.pow(totalVelocity,2) + math.pow(player.yPosition-yPosInitial,2));
+    assortedGameArrays.bullets.push(this);
 };
 
 function PlayerBullet (xPosInitial, xDirectionFacing, yDirectionFacing){
     this.xPosition = xPosInitial;
-    this.yPosition = 0;  // Should actually be Canvas max - 1/2 player image height to put the bottom of the image on the bottom of the page.
+    this.yPosition = 0;  // Should actually be gamax - 1/2 player image height to put the bottom of the image on the bottom of the page.
     this.yVelocity = 'TBD';  // Need to work out a good bullet velocity
     this.xVelocity = 0; //  Will be something else if we reach stretch goal of aiming with mouse.  
+    assortedGameArrays.playerBullets.push(this);
 };
 
