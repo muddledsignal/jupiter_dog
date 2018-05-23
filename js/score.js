17c4cf8747e50d04ba94c0f290996bad5f9bbaf0
Jane Smith <example@email.com>
2018-05-23 16:46:48 -0700

    added table header to score page

--- js/score.js
+++ js/score.js
@@ -4,32 +4,51 @@
 
 var allPlayer = [];
 
+function createTable() {
+    function BaseScore(name, score) {
+        this.name = name;
+        this.score = score;
+        allPlayer.push(this);
+    }
 
-function BaseScore(name, score) {
-    this.name = name;
-    this.score =score;
-    allPlayer.push(this);
+    new BaseScore("bob", 1000000);
+    new BaseScore("andy", 10000);
+    new BaseScore("ryan", 100);
+    new BaseScore("sarah", 10);
+    new BaseScore("jill", 1);
+    new BaseScore("mary", 20);
 }
-
-new BaseScore("bob", 1000000);
-new BaseScore("andy", 10000);
-new BaseScore("ryan", 100);
-new BaseScore("sarah", 10);
-new BaseScore("jill", 1);
-new BaseScore("mary", 20);
-
-var cells = document.getElementById('score');
-
-for (var i = 0; i < 5; i++){
+function fillTable() {
+    var cells = document.getElementById('score');
     var newTr = document.createElement('tr');
-    var newTd = document.createElement('td');
-    var newTd2 = document.createElement('td');
+    var th = document.createElement('th');
+    var th2 = document.createElement('th');
 
-    newTd.textContent = allPlayer[i].name;
-    newTd2.textContent = allPlayer[i].score;
+    
+    th.textContent = 'Name';
+    th2.textContent = 'Score';
+    newTr.append(th);
+    newTr.append(th2);
+    cells.append(newTr);
+    
+    for (var i = 0; i < 5; i++) {
+        
+        var newTr = document.createElement('tr');
+        var newTd = document.createElement('td');
+        var newTd2 = document.createElement('td');
 
-    newTr.appendChild(newTd);
-    newTr.appendChild(newTd2);
+        document.createElement('tr');
+        newTd.textContent = allPlayer[i].name;
+        newTd2.textContent = allPlayer[i].score;
 
-    cells.appendChild(newTr);
+
+        newTr.append(newTd);
+        newTr.append(newTd2);
+
+        cells.append(newTr);
+    }
 }
+//Make sure to cal functions above
+createTable();
+fillTable();
+//CREATE A FORM WITH EVENT LISTENER
